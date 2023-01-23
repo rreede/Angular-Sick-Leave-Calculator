38a6216e3ba97eba1a3106a50022204d7e7e598e
Rene Reede <rene.reede@gmail.com>
2023-01-23 23:10:21 +0200

    65

--- Angular-Web-Expert/src/app/app.module.ts
+++ Angular-Web-Expert/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CompensationCalculatorComponent } from './compensation-calculator/compensation-calculator.component'
+import { CompensationCalculatorComponent } from './compensation-calculator/compensation-calculator.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompensationCalculatorComponent
+    CompensationCalculatorComponent,
   ],
   imports: [
     BrowserModule,
--- Angular-Web-Expert/src/app/compensation-calculator/compensation-calculator.component.ts
+++ Angular-Web-Expert/src/app/compensation-calculator/compensation-calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-compensation-calculator',
@@ -6,5 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['../app.component.css']
 })
 export class CompensationCalculatorComponent {
-  employercompensation = 42;
+  
+  /* Inputs */ 
+
+  income: number = 0;
+  daysOnSickLeave: number = 0;
+  hasTubercolosis: boolean = false;
+
+  /* Calculations */
+
+  employerCompensatesDays: number = 0;
+  healthInsuranceCompensatesDays: number = 0;
+  employerCompensatesAmount: number = 0;
+  healthInsuranceCompensatesAmount: number  = 0;
+  compensationTotal: number  = 0;
+
+  maximumDurationInsurance:number = 182;
+  maximumDurationInsuranceTuberculosis:number = 240;
+  percentageOfIncome = (0.7 * this.income);
+
+  // Constants
+  amountOfDaysNotCompensated: number = 3;
+  maxAmountOfDaysCompensatedByEmployer: number = 5;
+  maxAmountOfDaysWhereHealthInsuranceDoesntCompensate: number = 8;
+  listOfAverageWorkingDaysInAMonth: number[] = [19, 20, 21, 22];
+  
+  
+
+  onCalculateCompensation() {
+    const averageWorkingDaysInAMonth: number = Math.ceil(this.listOfAverageWorkingDaysInAMonth.reduce((a, b) => {
+      return a + b;
+    }, 0) / 4)
+    // 3 or Less Days No compensation
+    if (this.daysOnSickLeave < this.amountOfDaysNotCompensated + 1)  {
+      this.employerCompensatesDays = 0;
+      this.healthInsuranceCompensatesDays = 0;
+    // Estonian health insurance pays
+    } else {
+
+      this.employerCompensatesDays = this.daysOnSickLeave > this.amountOfDaysNotCompensated  && this.daysOnSickLeave <= this.maxAmountOfDaysWhereHealthInsuranceDoesntCompensate ?  this.daysOnSickLeave - this.amountOfDaysNotCompensated : this.maxAmountOfDaysCompensatedByEmployer; 
+
+      this.healthInsuranceCompensatesDays = this.employerCompensatesDays === this.maxAmountOfDaysCompensatedByEmployer ? this.daysOnSickLeave - this.maxAmountOfDaysWhereHealthInsuranceDoesntCompensate : this.daysOnSickLeave - this.amountOfDaysNotCompensated - this.employerCompensatesDays; 
+
+      const paidDaily: number = this.percentageOfIncome / averageWorkingDaysInAMonth;
+      this.employerCompensatesAmount = this.employerCompensatesDays * paidDaily; 
+
+      this.healthInsuranceCompensatesAmount = this.employerCompensatesDays === this.maxAmountOfDaysCompensatedByEmployer ? (this.daysOnSickLeave - this.employerCompensatesDays) * paidDaily : (this.daysOnSickLeave - this.amountOfDaysNotCompensated - this.employerCompensatesDays) * paidDaily ; 
+
+      this.compensationTotal = 0;
+
+      (averageWorkingDaysInAMonth);
+      
+    }
+
+  }
 }
