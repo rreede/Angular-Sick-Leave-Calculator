eb9938f5e619ce8e131c9308016e1a1090ac4a90
Rene Reede <rene.reede@gmail.com>
2023-01-27 14:11:26 +0200

    Moved component stylings to correct place

--- Angular-Web-Expert/src/app/compensation-calculator/compensation-calculator.component.ts
+++ Angular-Web-Expert/src/app/compensation-calculator/compensation-calculator.component.ts
@@ -7,13 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompensationCalculatorComponent {
   
-  /* Inputs */ 
+  // Inputs
 
   income: number = 0;
   daysOnSickLeave: number = 0;
   hasTubercolosis: boolean = false;
 
-  /* Calculations */
+  // Calculations
 
   employerCompensatesDays: number = 0;
   healthInsuranceCompensatesDays: number = 0;
@@ -24,6 +24,10 @@ export class CompensationCalculatorComponent {
   maximumDurationInsurance:number = 182;
   maximumDurationInsuranceTuberculosis:number = 240;
   percentageOfIncome = (0.7 * this.income);
+  totalDaysCompensated: number = 0;
+  employerDailyAllowance: number = 0;
+  InsuranceDailyAllowance: number = 0;
+  
 
   // Constants
   amountOfDaysNotCompensated: number = 3;
@@ -31,31 +35,57 @@ export class CompensationCalculatorComponent {
   maxAmountOfDaysWhereHealthInsuranceDoesntCompensate: number = 8;
   listOfAverageWorkingDaysInAMonth: number[] = [19, 20, 21, 22];
   
-  
-
   onCalculateCompensation() {
+
+    
     const averageWorkingDaysInAMonth: number = Math.ceil(this.listOfAverageWorkingDaysInAMonth.reduce((a, b) => {
       return a + b;
     }, 0) / 4)
+
     // 3 or Less Days No compensation
+
     if (this.daysOnSickLeave < this.amountOfDaysNotCompensated + 1)  {
       this.employerCompensatesDays = 0;
       this.healthInsuranceCompensatesDays = 0;
-    // Estonian health insurance pays
+      
     } else {
 
+      // Estonian health insurance pays
+
+      // How many days employer compensates
       this.employerCompensatesDays = this.daysOnSickLeave > this.amountOfDaysNotCompensated  && this.daysOnSickLeave <= this.maxAmountOfDaysWhereHealthInsuranceDoesntCompensate ?  this.daysOnSickLeave - this.amountOfDaysNotCompensated : this.maxAmountOfDaysCompensatedByEmployer; 
 
-      this.healthInsuranceCompensatesDays = this.employerCompensatesDays === this.maxAmountOfDaysCompensatedByEmployer ? this.daysOnSickLeave - this.maxAmountOfDaysWhereHealthInsuranceDoesntCompensate : this.daysOnSickLeave - this.amountOfDaysNotCompensated - this.employerCompensatesDays; 
+      // How many days health insurance compensates
+
+      this.healthInsuranceCompensatesDays = this.employerCompensatesDays === this.maxAmountOfDaysCompensatedByEmployer ? this.daysOnSickLeave - this.maxAmountOfDaysWhereHealthInsuranceDoesntCompensate : this.daysOnSickLeave - this.amountOfDaysNotCompensated - this.employerCompensatesDays;
+
+      // How much worker is paid daily (70% of gross income)
 
       const paidDaily: number = this.percentageOfIncome / averageWorkingDaysInAMonth;
-      this.employerCompensatesAmount = this.employerCompensatesDays * paidDaily; 
+
+      // Amount employer compensates
+
+      this.employerCompensatesAmount = this.employerCompensatesDays * paidDaily;
+
+      // Amount health insurance compensates
 
       this.healthInsuranceCompensatesAmount = this.employerCompensatesDays === this.maxAmountOfDaysCompensatedByEmployer ? (this.daysOnSickLeave - this.employerCompensatesDays) * paidDaily : (this.daysOnSickLeave - this.amountOfDaysNotCompensated - this.employerCompensatesDays) * paidDaily ; 
+      
+      // Sum of the days being compensated
 
-      this.compensationTotal = 0;
+      this.totalDaysCompensated = this.employerCompensatesDays + this.healthInsuranceCompensatesDays;
 
-      (averageWorkingDaysInAMonth);
+      // Employers Daily Allowance
+
+      this.employerDailyAllowance = this.employerCompensatesAmount / this.employerCompensatesDays;
+
+      // Health Insurance Daily Allowance
+
+      this.InsuranceDailyAllowance = this.healthInsuranceCompensatesAmount / this.healthInsuranceCompensatesDays;
+
+      // Compenstation total
+
+      this.compensationTotal = this.employerCompensatesAmount + this.healthInsuranceCompensatesAmount;
       
     }
 
